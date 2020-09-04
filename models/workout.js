ebe1ed87cc752518d68dc483857edc6d390e29aa
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-03 20:17:31 -0400

    created htmlroutes.js file and workout.js on models file

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: Date,
+  exercises: [
+      {
+        type: {
+            type:String,
+            trim: true,
+            required:true
+        },
+        name:{
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration:{
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+      }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -15,14 +15,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+require("./routes/htmlroutes")(app);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser:true,
     useFindAndModify: false
 });
 //Here are we are setting up routes
 
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 })
+
