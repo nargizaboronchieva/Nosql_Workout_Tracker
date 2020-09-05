e15f800a2856dadfce95d8cb5a2a52bd8819a16c
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-05 11:08:29 -0400

    added api.js in routes folder

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,31 @@
-const Workout = require ()
+var express = require('express')
+var router = express.Router()
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+  })
+  // define the home page route
+  router.get('/workouts', function (req, res) {
+      Workout.find()
+        .then(data => {
+          res.json(data);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+  });
+  router.post("/api/workouts", function(req, res) {
+    Workout.create({})
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log("err", err);
+        res.json(err);
+      });
+  });
+ // define the about route 
+ router.get('/about', function (req, res) {
+    res.send('About birds')
+  })
+  module.exports = router;
+  
