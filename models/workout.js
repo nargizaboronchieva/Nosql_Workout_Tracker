d25860d5a8f99d7c70718422402726fd02ee0339
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-07 15:15:10 -0400

    debugging on workout.js total workout duration was showing as undefined.

--- models/workout.js
+++ models/workout.js
@@ -1,39 +1,49 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
+const ExerciseSchema = new Schema({
+  name: {
+    type: String,
   },
-  exercises: [{
-    type: {
-      type: String,
-      trim: true,
-      required: 'exercise field cannot be blank.'
-    },
-    name: {
-      type: String,
-      required: 'enter a name for exercise.'
-    },
-    duration: {
-      type: Number
-    },
-    weight: {
-      type: Number
-    },
-    reps: {
-      type: Number
-    },
-    sets: {
-      type: Number
-    },
-    distance: {
-      type: Number
-    }
-  }]
+  type: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+  },
+  sets: {
+    type: Number,
+  },
+  reps: {
+    type: Number,
+  },
+  duration: 
+    {
+    type: Number,
+  },
+  distance: {
+    type: Number,
+  },
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date, 
+    default: Date.now,
+  },
+  exercises: [ExerciseSchema],
+}, { toJSON: {
+    virtuals: true
+} }
+);
+WorkoutSchema.virtual('totalDuration').get(function() {
+    return this.exercises.reduce((total, exerecise) => {
+        return total + exerecise.duration
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
+//reduce
