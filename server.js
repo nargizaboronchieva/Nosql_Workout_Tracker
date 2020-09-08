c7cecfd5ee26e7832b7a928ca7e79bfb845ae17d
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-08 18:13:04 -0400

    heroku set up

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(morgan("tiny"))
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true
 });
 
